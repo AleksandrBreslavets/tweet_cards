322c2251155961b9967b97ff09282e48136cdb3f
AleksandrBreslavets <118082386+AleksandrBreslavets@users.noreply.github.com>
2023-10-14 13:01:03 +0000

    Deploying to gh-pages from @ AleksandrBreslavets/tweet_cards@1c73fcef299d6c865e1cf2daa9aaf809f0802450 🚀

---
+++ static/js/544.53e6d13c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[544],{544:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("p",{children:"Home page"})}}}]);
+//# sourceMappingURL=544.53e6d13c.chunk.js.map
